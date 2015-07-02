8c4253309ebaec61f3585696d16f850d9a9ccf73
Zhou Hao <rmusique@gmail.com>
2015-07-02 12:26:01 +0800

    add desc for swagger

--- lib/UserRouter.ex
+++ lib/UserRouter.ex
@@ -1,21 +1,15 @@
 defmodule UserRouter do
   use Maru.Router
 
-  # defmodule UserEntity do
-  #   def serialize(user, _opts) do
-  #     user
-  #   end
-  # end
-
   namespace :user do
+    desc "get user info by id"
     params do
-      requires :id, type: String
-      requires :age, type: Integer, values: 18..65
-      requires :sex, type: Atom, values: [:male, :female], default: :male
+      requires :id, type: String, desc: "user id"
+      optional :age, type: Integer, values: 18..65, desc: "age [18-65]"
+      optional :sex, type: Atom, values: [:male, :female], default: :male, desc: "male, female"
     end
     get do
-      user = %{ uid: params[:id], age: params[:age], sex: params[:sex] }
-      #present user, with: UserEntity
+      %{ uid: params[:id], age: params[:age], sex: params[:sex] }
     end
   end
 end
